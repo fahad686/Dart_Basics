c56f5e8361d87a17b8f738d10cc9f18a8d1f747c
Fahad Ali <fahad.ali@schuja.com>
2025-07-12 03:25:58 +0500

    sorting of String Custom  list

--- sorting/basics_sorting.dart
+++ sorting/basics_sorting.dart
@@ -17,12 +17,52 @@ void main(){
       User('Mehtab',22),
     ];
 
-    //Ascending
+    //Ascending    
+      print("Ascending Ordre");
    users.sort((a,b)=>a.age.compareTo(b.age));
    for(int i=0;i<users.length;i++){
     print("Name: ${users[i].name} - Age: ${users[i].age}");
    }
     
+    //Descending order
+          print("Descening Ordre");
+    users.sort((a,b)=>b.age.compareTo(a.age));
+    for(var user in users){
+      print('Name:${user.name} - age: ${user.age}');
+
+    }
+
+
+    /*Output 
+    Ascending: [1, 2, 5, 7, 9]
+Decending: [9, 7, 5, 2, 1]
+Ascending Ordre
+Name: Mehtab - Age: 22
+Name: Ali - Age: 24
+Name: Fahad - Age: 27
+Descening Ordre
+Name:Fahad - age: 27
+Name:Ali - age: 24
+Name:Mehtab - age: 22
+*/
+
+ List<String> fruits = ['banana', 'apple', 'orange', 'grape'];
+
+  // Sort in ascending order (default)
+  fruits.sort();
+  print('Ascending order: $fruits'); // Output: [apple, banana, grape, orange]
+
+  // Sort in descending order
+  fruits.sort((a, b) => b.compareTo(a));
+  print('Descending order: $fruits'); // Output: [orange, grape, banana, apple]
+
+  // Sort by string length
+  fruits.sort((a, b) => a.length.compareTo(b.length));
+  print('Sorted by length: $fruits'); // Output: [grape, apple, banana, orange]
+
+ // Sort by string length
+  fruits.sort((a, b) => b.length.compareTo(a.length));
+  print('Sorted by length: $fruits'); // Output: [orange, banana, grape, apple]
 }
 
 
